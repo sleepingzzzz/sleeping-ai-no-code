e4d698f080d4908d95d84bca5a40d6432bd41684
sleeping <sleepingzwy@outlook.com>
2025-07-27 12:37:58 +0800

    后端初始化
    1、初始化项目
    2、添加依赖和配置（Hutool 和 Knife4j 接口文档）
    3、提供基础代码（自定义异常、请求响应包装类）
    4、提供基础接口（健康检查接口）

---
+++ src/main/java/com/sleeping/sleepingainocode/SleepingAiNoCodeApplication.java
@@ -0,0 +1,13 @@
+package com.sleeping.sleepingainocode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleepingAiNoCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SleepingAiNoCodeApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/sleeping/sleepingainocode/controller/HealthController.java
@@ -0,0 +1,17 @@
+package com.sleeping.sleepingainocode.controller;
+
+import com.sleeping.sleepingainocode.common.BaseResponse;
+import com.sleeping.sleepingainocode.common.ResultUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+    @GetMapping("/")
+    public BaseResponse<String> healthCheck() {
+        return ResultUtils.success("ok");
+    }
+}
