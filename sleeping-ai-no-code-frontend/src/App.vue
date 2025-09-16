81c7769da997eb116c4428c8a288d4c2125c79fd
sleeping <sleepingzwy@outlook.com>
2025-09-17 05:11:34 +0800

    更改ico图标

--- sleeping-ai-no-code-frontend/src/App.vue
+++ sleeping-ai-no-code-frontend/src/App.vue
@@ -1,12 +1,12 @@
-<script setup lang="ts">
+<script lang="ts" setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
+    <img alt="Vue logo" class="logo" height="125" src="@/assets/logo.svg" width="125" />
+    <a-button type="primary">Primary Button</a-button>
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
 
