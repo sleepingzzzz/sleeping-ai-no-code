2fd4f2f4a199dfb335148f80f8312acfd020e424
sleeping <sleepingzwy@outlook.com>
2025-09-19 03:38:59 +0800

    用户模块后端
    1. 开发 MyBatis Flex 代码生成器
    2. 开发核心功能，用户注册、用户登录、获取登陆用户信息、用户注销等
    3. 全局权限校验 AOP
    4. 开发管理员增删改查接口
    5. 自定义配置解决丢失精度问题

--- src/main/java/com/sleeping/sleepingainocode/service/UserService.java
+++ src/main/java/com/sleeping/sleepingainocode/service/UserService.java
@@ -1,7 +1,14 @@
 package com.sleeping.sleepingainocode.service;
 
+import com.mybatisflex.core.query.QueryWrapper;
 import com.mybatisflex.core.service.IService;
+import com.sleeping.sleepingainocode.model.dto.UserQueryRequest;
 import com.sleeping.sleepingainocode.model.entity.User;
+import com.sleeping.sleepingainocode.model.vo.LoginUserVO;
+import com.sleeping.sleepingainocode.model.vo.UserVO;
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.util.List;
 
 /**
  * 用户 服务层。
@@ -19,6 +26,62 @@ public interface UserService extends IService<User> {
      * @return 新用户 id
      */
     long userRegister(String userAccount, String userPassword, String checkPassword);
+
+    /**
+     * 获取脱敏的已登录用户信息
+     *
+     * @return
+     */
+    LoginUserVO getLoginUserVO(User user);
+
+    /**
+     * 用户登录
+     *
+     * @param userAccount  用户账户
+     * @param userPassword 用户密码
+     * @param request
+     * @return 脱敏后的用户信息
+     */
+    LoginUserVO userLogin(String userAccount, String userPassword, HttpServletRequest request);
+
+    /**
+     * 获取当前登录用户
+     *
+     * @param request
+     * @return
+     */
+    User getLoginUser(HttpServletRequest request);
+
+    /**
+     * 获取脱敏后的用户信息
+     *
+     * @param user 用户信息
+     * @return
+     */
+    UserVO getUserVO(User user);
+
+    /**
+     * 获取脱敏后的用户信息（分页）
+     *
+     * @param userList 用户列表
+     * @return
+     */
+    List<UserVO> getUserVOList(List<User> userList);
+
+    /**
+     * 用户注销
+     *
+     * @param request
+     * @return 退出登录是否成功
+     */
+    boolean userLogout(HttpServletRequest request);
+
+    /**
+     * 根据查询条件构造数据查询参数
+     * @param userQueryRequest
+     * @return
+     */
+    QueryWrapper getQueryWrapper(UserQueryRequest userQueryRequest);
 
     /**
      * 加密
